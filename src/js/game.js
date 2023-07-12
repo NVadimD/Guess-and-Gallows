b6965cc94119ca5dc8bcf5b7c18e2234d6dac4a3
NVadimD <vadim.zelep@gmail.com>
2023-07-12 21:02:06 +0700

    начал писать логику игры

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,25 @@
+import { WORDS } from "./consts"
+
+const gameDiv = document.getElementById('game');
+
+const createPlaceholdersHTML = () => {
+    const word = sessionStorage.getItem('word');
+
+    const wordArray = Array.from(word);
+    // const arr2 = Array.from('_'.repeat(word.length));
+
+    const placeholdersHTML = wordArray.reduce((acc, curr, i) => acc + `<h1 id="letter_${i}" class='letter'>_</h1>`, '')
+
+    // let placeholdersHTML = '';
+    // for (let i = 0; i < word.length; i++){
+    //     placeholdersHTML += `<h1 id="letter_${i}" class='letter'>_</h1>`;
+    // }
+    return `<div id='placeholders' class='placeholders-wrapper'>${placeholdersHTML}</div>`
+}
+
 export const startGame = () => {
-    
+    const randomInd = Math.floor(Math.random()*WORDS.length);
+    const wordToGuess = WORDS[randomInd];
+    sessionStorage.setItem('word', wordToGuess);
+    gameDiv.innerHTML = createPlaceholdersHTML();
 }
