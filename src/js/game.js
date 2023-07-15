5ab21656fdf98c011aff9e2be85e4ff3ba6439f7
NVadimD <vadim.zelep@gmail.com>
2023-07-15 15:32:27 +0700

    Развернул деплой

--- src/js/game.js
+++ src/js/game.js
@@ -42,20 +42,19 @@ const createGallowsImg = () => {
     return image;
 };
 
-
 const checkLetter = (letter) => {
-    const word = sessionStorage.getItem('word');
+    const word = sessionStorage.getItem("word");
     const inputLetter = letter.toLowerCase();
     if (!word.includes(inputLetter)) {
-        const triesCounter = document.getElementById('tries-left');
+        const triesCounter = document.getElementById("tries-left");
         triesLeft -= 1;
         triesCounter.innerText = triesLeft;
 
-        const gallowsImg = document.getElementById('gallows-img');
+        const gallowsImg = document.getElementById("gallows-img");
         gallowsImg.src = `images/hg-${10 - triesLeft}.png`;
 
         if (triesLeft === 0) {
-            stopGame('lose');
+            stopGame("lose");
         }
     } else {
         const wordArray = Array.from(word);
@@ -63,41 +62,41 @@ const checkLetter = (letter) => {
             if (currentLetter === inputLetter) {
                 winCount++;
                 if (winCount === word.length) {
-                    stopGame('win');
+                    stopGame("win");
                     return;
                 }
-                document.getElementById(`letter_${i}`).innerText = inputLetter.toUpperCase();
+                document.getElementById(`letter_${i}`).innerText =
+                    inputLetter.toUpperCase();
             }
-        })
+        });
     }
-}
-
-
+};
 
 const stopGame = (status) => {
+    document.getElementById("placeholders").remove();
+    document.getElementById("tries").remove();
+    document.getElementById("keyboard").remove();
+    document.getElementById("quit").remove();
 
-    document.getElementById('placeholders').remove();
-    document.getElementById('tries').remove();
-    document.getElementById('keyboard').remove();
-    document.getElementById('quit').remove();    
+    const word = sessionStorage.getItem("word");
 
-    const word = sessionStorage.getItem('word');
-
-    if (status === 'win') {
-        document.getElementById('gallows-img').src = 'images/hg-win.png';
-        document.getElementById('game').innerHTML += '<h2 class="result-header win">You won!</h2>'
-    } else if (status === 'lose') {
-        document.getElementById('game').innerHTML += '<h2 class="result-header lose">You lost :(</h2>'
-    } else if (status === 'quit') {
-        logoH1.classList.remove('logo-sm');
-        document.getElementById('gallows-img').remove();
+    if (status === "win") {
+        document.getElementById("gallows-img").src = "images/hg-win.png";
+        document.getElementById("game").innerHTML +=
+            '<h2 class="result-header win">You won!</h2>';
+    } else if (status === "lose") {
+        document.getElementById("game").innerHTML +=
+            '<h2 class="result-header lose">You lost :(</h2>';
+    } else if (status === "quit") {
+        logoH1.classList.remove("logo-sm");
+        document.getElementById("gallows-img").remove();
     }
 
-    document.getElementById('game').innerHTML += `<p>The word was <span class="result-word">${word}</span></p><button id="play-again" class="button-primary px-5 py-2 mt-5">Play again</button>`;
-    document.getElementById('play-again').onclick = startGame;
-}
-
-
+    document.getElementById(
+        "game"
+    ).innerHTML += `<p>The word was <span class="result-word">${word}</span></p><button id="play-again" class="button-primary px-5 py-2 mt-5">Play again</button>`;
+    document.getElementById("play-again").onclick = startGame;
+};
 
 export const startGame = () => {
     triesLeft = 10;
@@ -114,7 +113,7 @@ export const startGame = () => {
 
     const keyboardDiv = createKeyboard();
     keyboardDiv.addEventListener("click", function (event) {
-        if (event.target.tagName === 'BUTTON'){
+        if (event.target.tagName === "BUTTON") {
             event.target.disabled = true;
             checkLetter(event.target.id);
         }
@@ -125,11 +124,16 @@ export const startGame = () => {
 
     gameDiv.appendChild(keyboardDiv);
 
-    gameDiv.insertAdjacentHTML('beforeend', '<button id="quit" class="button-secondary px-2 py-1 mt-4">Quit</button>');
-    document.getElementById('quit').onclick = () => {
-        const isSure = confirm('Are you sure you want to quit and lose progress?');
+    gameDiv.insertAdjacentHTML(
+        "beforeend",
+        '<button id="quit" class="button-secondary px-2 py-1 mt-4">Quit</button>'
+    );
+    document.getElementById("quit").onclick = () => {
+        const isSure = confirm(
+            "Are you sure you want to quit and lose progress?"
+        );
         if (isSure) {
-            stopGame('quit');
+            stopGame("quit");
         }
-    }
+    };
 };
